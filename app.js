a9cbaa87d2775cff4c6f0240584289568f74d5c7
Kushal Chhetri <71422133+kushal-chhetri7@users.noreply.github.com>
2023-11-06 17:49:56 +0600

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
-}));
+})); 
 
 app.use(session({
     secret: "Our little secret.",
