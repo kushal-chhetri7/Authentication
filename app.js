6a71bc9af9c68b92a3d795ba4f8250f214bc731a
Kushal Chhetri <71422133+kushal-chhetri7@users.noreply.github.com>
2024-09-14 17:32:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -45,7 +45,6 @@ passport.use(User.createStrategy());
 passport.serializeUser(function(user, done) {
     done(null, user.id);
 });
-
 passport.deserializeUser(function(id, done) {
     User.findById(id, function(err, user) {
         done(err, user);
